0a7a9ecc45927aa2b045c581ea0e3bc5b5a67108
Dane-Dawson <Dane.P.Dawson@gmail.com>
2020-01-30 13:14:45 -0600

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song
+    attr_accessor :name, :artist, :genre
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def save
+        @@all << self
+    end
+
+    def artist_name
+        if self.artist
+           self.artist.name
+        else
+            return nil
+        end
+    end
+end
